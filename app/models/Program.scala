3abe949b458e28ca7d24493ad170c1b59e2fd8f6
Marcelo Aliquintuy <mmaliq@gmail.com>
2013-01-15 11:28:08 -0300

    Change Program list from mongoTemplate to CrudRepository like Schedules

--- app/models/Program.scala
+++ app/models/Program.scala
@@ -4,20 +4,25 @@ import play.modules.spring.Spring
 import org.springframework.data.mongodb.core._
 import org.springframework.data.mongodb.core.query._
 import scala.collection.JavaConversions._
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.mongodb.core.mapping.Document
+import org.bson.types.ObjectId
 
-case class Program(id: Long, name: String,description:String,episode:String)
+@Document(collection="programs")
+case class Program(id: String, name: String,description:String,episode:String)
+
+trait ProgramRepository extends CrudRepository[Program,String]
 
 object Program {
 
   def all(): List[Program] = {
-    val mongoOperations: MongoOperations = Spring.getBean("mongoTemplate").asInstanceOf[MongoOperations]
-    val results = mongoOperations.find(
-        new Query(Criteria where("name") is("House")),classOf[Program],"programs")
-    val programs:List[Program] = asScalaBuffer(results).toList
+    val pr= Spring.getBeanOfType(classOf[ProgramRepository])
+    // val mongoOperations: MongoOperations = Spring.getBean("mongoTemplate").asInstanceOf[MongoOperations]
+    // val results = mongoOperations.find(
+    //     new Query(Criteria where("name") is("House")),classOf[Program],"programs")
+    val programs:List[Program] = pr.findAll().toList
     programs
     }
 
   def create(label: String) {}
-
-  def delete(id: Long) {}
 }
--- app/models/Schedule.scala
+++ app/models/Schedule.scala
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.core.query._
 import scala.collection.JavaConversions._
 import org.bson.types.ObjectId
 import java.util.Date
-import java.lang.Long
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.mongodb.core.mapping.Document
 
@@ -19,12 +18,6 @@ object Schedule {
 
   def all(): List[Schedule] = {
     val sr = Spring.getBeanOfType(classOf[ScheduleRepository])
-
-
-    // val mongoOperations: MongoOperations = Spring.getBean("mongoTemplate").asInstanceOf[MongoOperations]
-    // val results = mongoOperations.find(
-    //     new Query(Criteria where("duration") is(60)),classOf[Schedule],"schedules")
-    // val schedules:List[Schedule] = asScalaBuffer(results).toList
     val schedules:List[Schedule]  = sr.findAll().toList
     schedules
     }
